54ded6d6362a7d531a68fc03ce0e15c595d1a81a
MarlenZeyn <zmarlen45@gmail.com>
2023-02-23 14:19:45 +0600

    gateway2

---
+++ src/main/java/kz/dar/academy/postofficeapigateway/PostofficeApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package kz.dar.academy.postofficeapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostofficeApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostofficeApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/dar/academy/postofficeapigateway/PostofficeApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dar.academy.postofficeapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostofficeApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
